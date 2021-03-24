2dcb4d5684ed3db0b5e708d25bc25e474a1e7106
Aleksandr Repeckiy <repetskiyaleksandr@gmail.com>
2021-03-24 10:44:28 +0200

    стили

--- jewellerycard/src/views/About.vue
+++ jewellerycard/src/views/About.vue
@@ -1,5 +1,120 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<div class="wrapper wrapper--pages">
+    <section class="about">
+        <h2 class="h2">Ювелирный Дом</h2>
+        <ul class="about__list">
+            <li class="about__item">
+                <div class="about__desc">
+                    <div class="row">
+                        <div class="col8 middle">
+                            <p>Период конца XIX – начала ХХ века принес миру экспериментальные жанры в литературе, промышленную революцию и либеральные ценности в системе образования. Ну, а ювелирному миру этот период подарил новые эстетические взгляды на самое драгоценное искусство. Свой  вклад в развитие ювелирного дела внесли и украинские мастера.</p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="about__item">
+                <div class="about__img">
+                    <div class="row">
+                        <div class="col6 hide">
+                            <img src="/img/about/about_1.jpg" alt="about1">
+                        </div>
+                        <div class="col6">
+                            <img src="/img/about/about_2.jpg" alt="about1">
+                        </div>
+                    </div>
+                </div>
+                <div class="about__desc">
+                    <div class="row">
+                        <div class="col8 middle">
+                            <p>Будущая столица Украины - Киев, уже тогда был одним из центров ювелирного искусства. В 1913 в городе работало 38  магазинов и мастерских, которые создавали украшения из серебра, золота и драгоценных камней. Почти все они были расположены на Крещатике и ближайших к нему улицах, которые буквально сияли блеском благородных бриллиантов, сапфиров, рубинов.</p>
+                            <p>Но, красный оттенок красит украшения, а не политические режимы. Диктатура «советов», насаждала потребителям массовый конвейерный продукт как в быту, так и в ювелирном искусстве . Зачастую, безликие низкосортные изделия выпускали монструозные заводы во главе с “Союзювелирпром”.</p>
+                            <p>Годы прошли, партийные верхушки отошли, режим пал. Люди, уставшие от почти столетнего художественного вакуума, вновь ощутили потребность в эстетике. Это вдохновило творцов ювелирного дома Jewellery Card на возрождение украинского высокого ювелирного искусства.</p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="about__item">
+                <div class="about__img">
+                    <div class="row">
+                        <div class="col4 hide">
+                            <img src="/img/about/about_6.jpg" alt="about1">
+                        </div>
+                        <div class="col4 hide">
+                            <img src="/img/about/about_7.jpg" alt="about1">
+                        </div>
+                        <div class="col4 ">
+                            <img src="/img/about/about_5.jpg" alt="about1">
+                        </div>
+                    </div>
+                </div>
+                <div class="about__desc">
+                    <div class="row">
+                        <div class="col8 middle">
+                            <p>История ювелирного дома Jewellery Card началась в 1999 году с небольшого ювелирного заказа, выполненного в стенах уютной семейной мастерской.  С того времени, уже двадцать лет, изо дня в день мы создаем нашу Вселенную украшений. Золотые творения ювелирного искусства  от Jewellery Card стали любимыми и самыми ожидаемыми среди украинских покупателей украшений. За два десятилетия работы, наш ювелирный дом вырос в настоящую семью с сотнями  сотрудников, непрестанно работающими над полным производственным циклом: от идеи и эскизов, до презентации готового сверкающего украшения клиенту. </p>
+                            <p>Сегодня, ювелирный дом Jewellery Card пишет новую страницу своей истории, работая над авторской коллекцией украшений с неповторимым дизайном. Переосмысляя лучшие практики и вековые традиции ювелирного дела, анализируя современные тренды ювелирной моды, наши мастера готовят к скорому выходу концептуальную коллекцию <a href="/ru/lemonnier"> LemoNNier</a>, посвященную светской эстетике нового времени.</p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="about__item">
+                <div class="about__img">
+                    <div class="row-irregular grid-md">
+                        <div class="row-irregular--left">
+                            <div class="col-irregular8 grid__item grid__hide">
+                                <img class="lazy" src="/img/about/about_8.jpg" alt="">
+                            </div>
+                            <div class="col-irregular6 grid__item">
+                                <img class="lazy" src="/img/about/about_10.jpg" alt="">
+                            </div>
+                            <div class="col-irregular6 grid__item">
+                                <img class="lazy" src="/img/about/about_11.jpg" alt="">
+                            </div>
+                        </div>
+                        <div class="row-irregular--right">
+                            <div class="col-irregular11 grid__item">
+                                <img class="lazy" src="/img/about/about_9.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="grid grid-sm">
+                        <div class="row">
+                            <div class="col4 hide grid__item">
+                                <img class="lazy" src="/img/about/about_11.jpg" alt="">
+                            </div>
+                            <div class="col4 hide grid__item ">
+                                <img class="lazy" src="/img/about/about_10.jpg" alt="">
+                            </div>
+                            <div class="col4 grid__item">
+                                <img class="lazy" src="/img/about/about_8-1.jpg" alt="">
+                            </div>
+                            <div class="col4 hide grid__item">
+                                <img class="lazy" src="/img/about/about_9-1.jpg" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="about__desc">
+                    <div class="row">
+                        <div class="col8 middle">
+                            <p>Дело всей жизни - это воздух, которым дышите 24/7. В модерновом офисе или в скромной квартире. С одним помощником или сотней. Работая с карандашом, воском и резцом или с прогрессивными системами виртуального 3D-моделирования. Детали не важны. Важно просто дышать - постоянно заниматься делом, которое станет продолжением Вас.</p>
+                            <p>Мы творим драгоценности, в которых тесно сплетены дух традиций и прогрессивность технологических  инноваций. </p>
+                            <p>В момент когда идеал, становится страстью - рождаются украшения Jewellery Card.</p>
+                        </div>
+                    </div>
+                </div>
+            </li>
+        </ul>
+        <div class="about__footer">
+            <div class="row">
+                <div class="col4 middle">
+                    <div class="about__footer-box">
+                        <h2 class="h2">Производство</h2>
+                        <p>Приоткрываем завесу над таинством создания ювелирных шедевров мастерами ювелирного дома Jewellery Card. Впервые за 19 лет.</p>
+                        <a class="btn" href="/ru/production">Детальнее</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
 </template>
